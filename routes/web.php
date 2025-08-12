d452ac60742d20b65d52f4bef938256967a6609d
punzalan223 <punzalan2233@gmail.com>
2025-08-13 01:31:38 +0800

    Add payment image

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Livewire\Volt\Volt;
 
 Volt::route('/' , 'home');
 Volt::route('/events' , 'events');
+Volt::route('/donate' , 'donate');
 Volt::route('/login' , 'login');
 Volt::route('/register' , 'register');
 
