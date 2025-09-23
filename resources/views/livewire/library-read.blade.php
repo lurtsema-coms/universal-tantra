486e1e2fd0d737d283a26d24c7e0246d6d8076fe
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-09-23 15:09:10 +0800

    library read

--- resources/views/livewire/library-read.blade.php
+++ resources/views/livewire/library-read.blade.php
@@ -11,18 +11,26 @@
         />
         <div class="grid sm:grid-cols-2 lg:grid-cols-2 gap-5 gap-y-10 !py-10">
             @for ($i=0; $i<4; $i++)                
-                <div class="rounded-4xl bg-gradient-to-b from-[#8C1717] to-[#541111] relative">
+                <div class="rounded-4xl bg-gradient-to-b from-[#8C1717] to-[#541111] relative cursor-pointer transform transition-transform duration-300 hover:scale-105">
                     <img
                         src="{{ asset('img/love-4.webp') }}" 
                         alt="Profile Picture"
                         class="w-full rounded-t-4xl h-56"
                     />
-                    <br>
-                    <x-frontend.c-header-md 
-                        :class="'text-white text-shadow-lg p-8 lg:text-3xl text-center '" 
-                        :message="'Start the Daily Tantric Breath'" 
-                    />
-                    <br>
+                    <div class="px-8 mt-5">
+                        <span class="uppercase font-garamond text-white">November 17 2020</span>
+                        <x-frontend.c-header-md 
+                            :class="'text-white text-shadow-lg  lg:text-3xl  '" 
+                            :message="'Start the Daily Tantric Breath'" 
+                        />
+                        <x-frontend.c-paragraph 
+                            :class="' text-white '"
+                            :message="
+                                'Sign up for our free breathing altar-30seconds of ritual delivered every morning'
+                            " 
+                        />
+                        <br>
+                    </div>
                 </div>
             @endfor
         </div>
