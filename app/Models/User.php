3ad820237c35ece8773db84833971561c88ac16c
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-09-24 01:50:20 +0800

    softdelete to users admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ namespace App\Models;
 
 use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory,SoftDeletes, Notifiable;
 
     /**
      * The attributes that are mass assignable.
